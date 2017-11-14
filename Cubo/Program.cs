cd9e59c8cc9cf604f571a6e670f0e0c6a5cacabf
NetoHaarp <NetoHaarp@LENOVO-PCNETO>
2017-11-13 22:19:09 -0600

    Agregar archivos de proyecto.

---
+++ Cubo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Programa
+{
+    class Cubo
+    {
+        public static void Main(String[] args)
+        {
+            double Profundidad = 0;
+            double Alto = 0;
+            double Largo = 0;
+            Console.WriteLine("Area de un Cubo");
+            Console.WriteLine("Profundidad");
+            Profundidad = double.Parse(Console.ReadLine());
+            Console.WriteLine("Alto");
+            Alto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Largo");
+            Largo = double.Parse(Console.ReadLine());
+            Alto = Profundidad * Alto * Largo;
+            Console.WriteLine("Area del cubo = " + Alto);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
